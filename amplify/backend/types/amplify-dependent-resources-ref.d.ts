dc9acb280c45cbfc3d3e6b190f9bf84ee59fd30e
Nontokozo <nontokozo92kunene@gmail.com>
2024-09-12 20:17:34 +0200

    timezone

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactamplifydemo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
